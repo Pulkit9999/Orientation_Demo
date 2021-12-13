e40c3b41c625e2f2d460edc9111625c1c1e251cf
unknown <pullkit.9@gmail.com>
2021-12-13 19:34:09 +0530

    OrientationDemo

---
+++ android/app/src/main/kotlin/com/example/orientation_demos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.orientation_demos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(OrientationFix());
+}
+
+class OrientationFix extends StatelessWidget {
+  const OrientationFix({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Orientation Test App',
+        home: Scaffold(
+            appBar: AppBar(title: Text("Orientation App")),
+            body: Row(
+              children: [DynamicWidget()],
+            )));
+  }
+}
+
+class DynamicWidget extends StatelessWidget {
+  const DynamicWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Orientation _orientation = MediaQuery.of(context).orientation;
+    return Center(
+      child: _orientation == Orientation.portrait
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.ac_unit_outlined, size: 80.0),
+                Icon(Icons.access_alarm_sharp, size: 80.0)
+              ],
+            )
+          : Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Icon(Icons.ac_unit_outlined, size: 75.0),
+            ]),
+    );
+  }
+}
